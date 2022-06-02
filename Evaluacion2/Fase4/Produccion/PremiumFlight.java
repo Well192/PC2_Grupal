db31a88bdd1fd3be2b28e805124d85682d9ce8dd
ARMANDO\Armando <armandolluengallardo@gmail.com>
2022-06-02 10:40:22 -0500

    pregunta 5

--- Evaluacion2/Fase4/Produccion/PremiumFlight.java
+++ Evaluacion2/Fase4/Produccion/PremiumFlight.java
@@ -1,6 +1,22 @@
 public class PremiumFlight extends Flight {
 
-  // Diseño inicial de la clase  PremiumFlight. Pregunta 5
+    // Diseño inicial de la clase  PremiumFlight. Pregunta 5
+
+    public PremiumFlight(String id) {
+        super(id);
+    }
+
+    @Override
+    public boolean addPassenger(Passenger passenger) {
+        return false;
+    }
+
+    @Override
+    public boolean removePassenger(Passenger passenger) {
+        return false;
+    }
+
+
 
 }
 
